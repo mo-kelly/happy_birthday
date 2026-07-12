2341077a0746a9e7c10dd9bfa7831621c75c2659
Max Kellner <maxkellner82@gmail.com>
2026-07-12 16:47:37 +0200

    adjusted to 15LED and brightness 50 (0-255)

--- scripts/led_udp_bridge.py
+++ scripts/led_udp_bridge.py
@@ -18,11 +18,11 @@ import sys
 from rpi_ws281x import Color, PixelStrip
 
 # --- Konfiguration: an eigene Hardware anpassen ---
-LED_COUNT = 150        # Anzahl LEDs im Streifen
+LED_COUNT = 15        # Anzahl LEDs im Streifen
 LED_PIN = 18            # GPIO18 (PWM0)
 LED_FREQ_HZ = 800000
 LED_DMA = 10
-LED_BRIGHTNESS = 200     # 0-255
+LED_BRIGHTNESS = 50     # 0-255
 LED_INVERT = False
 LED_CHANNEL = 0
 
